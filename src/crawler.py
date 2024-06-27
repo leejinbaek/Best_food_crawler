c85d3af30113bcf42fcb27d3726f9d97f9f5666f
leejinbaek <wlsqor22222@naver.com>
2024-06-27 21:20:51 +0900

    retreat

--- src/crawler.py
+++ src/crawler.py
@@ -6,33 +6,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 import time
-
+#옵션 설정
 options = Options()
 options.add_argument("--start-maximized")
 options.add_experimental_option("detach", True)
 
+#드라이버 설정
 driver = webdriver.Chrome(options=options)
 
-def crawler(keyword):
-    url = f"https://map.naver.com/p/search/{keyword}"
-    driver.get(url)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input_search")))
+#페이지 스크롤 함수
+def page_down(num):
+    body = driver.find_element(By.CSS_SELECTOR, ".XUrfU")
+    body.click()
+    
+    for i in range(num):
+        body.send_keys(Keys.PAGE_DOWN)
+        time.sleep(1)
 
-    searchbox = driver.find_element(By.ID, "input_search")
+#크롤러 작성
+def crawler(keyword):
+    
+    #url 접근
+    url = f"https://map.naver.com"
+    driver.get(url)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "input_search")))
+
+    #키워드 검색
+    searchbox = driver.find_element(By.CLASS_NAME, "input_search")
     searchbox.send_keys(f"{keyword}")
     time.sleep(2)
+    searchbox.send_keys(Keys.ENTER)
+    time.sleep(5)
     
-    searchbtn = driver.find_element(By.CLASS_NAME, "button_search")
-    searchbtn.click()
-    time.sleep(2)
-    
-    wheel_location = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "_pcmap_list_scroll_container")))
-    action = ActionChains(driver)
-    action.move_to_element(wheel_location).perform()
-    
-    for i in range(10):
-        action.scroll_by_amount(0,100).perform()
+    #페이지 스크롤 _____________--막힘
+    scrpage = driver.find_element(By.CLASS_NAME, "body.place_on_pcmap")
+    scrpage.click()
+    page_down(40)
     
     driver.quit()
 
